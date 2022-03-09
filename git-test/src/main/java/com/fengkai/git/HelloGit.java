d8f7259616cb15063fa4043443cb68d3d4595e9c
fengkai <21210240157@m.fudan.edu.cn>
2022-03-09 21:37:33 +0800

    初始

--- git-test/src/main/java/com/fengkai/git/HelloGit.java
+++
@@ -1,8 +0,0 @@
-package com.fengkai.git;
-
-public class HelloGit {
-    public static void main(String[] args) {
-        System.out.println("HelloGit11");
-        System.out.println("创建分支");
-    }
-}
